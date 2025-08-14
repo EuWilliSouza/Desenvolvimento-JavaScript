921801d61101955c55932fea85011d31f95041dd
Willian Souza Guilherme <williansouzaguilherme14@gmail.com>
2025-08-14 16:54:08 -0300

    14.08.25..

---
+++ Aula 11 - Condições Encadeadas/Operadores Logicos/Desafio Triangulo/script.js
@@ -0,0 +1,31 @@
+function mostrarResultado(){
+
+    let ladoA = parseInt(document.querySelector("#lado1").value)
+    let ladoB = parseInt(document.querySelector("#lado2").value)
+    let ladoC = parseInt(document.querySelector("#lado3").value)
+    let resultado = document.querySelector("#resultado")
+    let tipoTriangulo = document.querySelector("#tipoTriangulo")
+
+    let ladoAB = ladoA + ladoB
+    let ladoAC = ladoA + ladoC
+    let ladoBC = ladoB + ladoC
+
+if (ladoAB > ladoC && ladoAC > ladoB && ladoBC > ladoA){
+    resultado.innerHTML = "LADOS VÁLIDOS PARA FORMAR UM TRIÂNGULO";
+
+    if (ladoA == ladoB && ladoB == ladoC){
+    tipoTriangulo.innerHTML = "TRIÂNGULO EQUILÁTERO"; 
+    }
+    else if (ladoA == ladoB || ladoA == ladoC || ladoB == ladoC) {
+    tipoTriangulo.innerHTML = "TRIÂNGULO ISÓSCELES";    
+    }  
+    else if (ladoA != ladoB != ladoC) {
+    tipoTriangulo.innerHTML = "TRIÂNGULO ECALENO";    
+    }                
+}
+else {
+    resultado.innerHTML = "LADOS INVALIDOS PARA FORMAÇÃO DE UM TRIÂNGULO";
+}
+
+    
+}
